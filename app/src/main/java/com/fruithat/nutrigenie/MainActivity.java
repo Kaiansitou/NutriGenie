63207328e98c56b76276001a88669e2cb2412278
Nathan Palmer <npalmer4@umd.edu>
2018-11-08 20:07:30 -0500

    Fixed naming and app now runs.

---
+++ app/src/main/java/com/fruithat/nutrigenie/MainActivity.java
@@ -0,0 +1,71 @@
+package com.fruithat.nutrigenie;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_scan:
+                    mTextMessage.setText(R.string.title_scan);
+                    return true;
+                case R.id.navigation_account:
+                    mTextMessage.setText(R.string.title_account);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        final BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        navigation.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.navigation_home:
+                                break;
+                            case R.id.navigation_scan:
+                                break;
+                            case R.id.navigation_account:
+
+                                Button accountButton = navigation.findViewById(R.id.navigation_account);
+                                accountButton.setOnClickListener(new View.OnClickListener() {
+
+                                    public void onClick(View v) {
+                                        Intent intent = new Intent(MainActivity.this, AccountActivity.class);
+                                        startActivity(intent);
+                                    }
+                                });
+
+                        }
+                        return false;
+                    }
+                });
+    }
+}
