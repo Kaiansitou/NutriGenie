7640d3114658b1e9ea35205fb05d1a5507014ea0
William T Yeager <wyeager@terpmail.umd.edu>
2018-11-11 22:49:04 -0500

    Add 'Test Scan' button that detects text in a bitmap

--- app/src/main/java/com/fruithat/nutrigenie/ScanFragment.java
+++ app/src/main/java/com/fruithat/nutrigenie/ScanFragment.java
@@ -1,12 +1,35 @@
 package com.fruithat.nutrigenie;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Point;
+import android.graphics.Rect;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.text.FirebaseVisionText;
+import com.google.firebase.ml.vision.text.FirebaseVisionTextRecognizer;
+import com.google.firebase.ml.vision.text.RecognizedLanguage;
+
+import java.util.List;
 
 public class ScanFragment extends Fragment {
+
+    private FirebaseVisionImage image;
+    private Bitmap bitmap;
+    private FirebaseVisionTextRecognizer textRecognizer;
+    private Button testScanButton;
+
+    private static final String TAG = "NutriGenie";
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -23,6 +46,66 @@ public class ScanFragment extends Fragment {
 
         // Don't destroy Fragment on reconfiguration
         setRetainInstance(true);
+
+        bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.nutrition_label_large);
+        image = FirebaseVisionImage.fromBitmap(bitmap);
+
+        textRecognizer = FirebaseVision.getInstance()
+                .getOnDeviceTextRecognizer();
     }
 
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        testScanButton = getActivity().findViewById(R.id.scan_button);
+        testScanButton.setOnClickListener(new Button.OnClickListener(){
+            public void onClick(View v) {
+                processLabel();
+            }
+        });
+    }
+
+    private void processLabel() {
+        textRecognizer.processImage(image)
+                .addOnSuccessListener(new OnSuccessListener<FirebaseVisionText>() {
+                    @Override
+                    public void onSuccess(FirebaseVisionText result) {
+                        Log.i(TAG, "SUCCESS!");
+                        String resultText = result.getText();
+                        //Log.i(TAG, "Result text: " + resultText);
+                        for (FirebaseVisionText.TextBlock block: result.getTextBlocks()) {
+                            String blockText = block.getText();
+                            Float blockConfidence = block.getConfidence();
+                            List<RecognizedLanguage> blockLanguages = block.getRecognizedLanguages();
+                            Point[] blockCornerPoints = block.getCornerPoints();
+                            Rect blockFrame = block.getBoundingBox();
+                            for (FirebaseVisionText.Line line: block.getLines()) {
+                                String lineText = line.getText();
+                                Float lineConfidence = line.getConfidence();
+                                List<RecognizedLanguage> lineLanguages = line.getRecognizedLanguages();
+                                Point[] lineCornerPoints = line.getCornerPoints();
+                                Rect lineFrame = line.getBoundingBox();
+                                for (FirebaseVisionText.Element element: line.getElements()) {
+                                    String elementText = element.getText();
+                                    Float elementConfidence = element.getConfidence();
+                                    List<RecognizedLanguage> elementLanguages = element.getRecognizedLanguages();
+                                    Point[] elementCornerPoints = element.getCornerPoints();
+                                    Rect elementFrame = element.getBoundingBox();
+                                    //Log.i(TAG, elementText);
+                                }
+                                Log.i(TAG, lineText);
+                            }
+                            // Log.i(TAG, blockText);
+                        }
+                    }
+                })
+                .addOnFailureListener(
+                        new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.i(TAG, "FAILURE!");
+                                Log.i(TAG, e.getMessage());
+                            }
+                        });
+    }
 }
