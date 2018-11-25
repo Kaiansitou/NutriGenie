2bb67c7753be0e5154680ed2b7323c98d09fd824
Yan-Jen Lo <vhv1122@gmail.com>
2018-11-25 03:23:46 -0500

    Adding Circle Graph

---
+++ app/src/main/java/com/fruithat/nutrigenie/CircleGraphFragment.java
@@ -0,0 +1,32 @@
+package com.fruithat.nutrigenie;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Bundle;
+import android.support.v4.app.*;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import com.github.mikephil.charting.*;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class CircleGraphFragment {
+    @Override
+    public CircleGraphFragment() {
+        // Required empty public constructor
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_about, container, false);
+
+    }
+}
